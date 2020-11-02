a4ff8014a6b144a5f65a0cb5f0e254ee89fd5e84
jota-dev-src <73813177+jota-dev-src@users.noreply.github.com>
2020-11-02 19:12:59 +0100

    Update k8n-update.sh

--- k8s/node/bin/k8n-update.sh
+++ k8s/node/bin/k8n-update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+exec 1> >(logger -s -t $(basename $0)) 2>&1
 # · ---
 function verGet { [[ $# -gt 0 ]] && ( [[ -z "${2}" ]] && echo "${1}" || echo "$(cat ${1})" ) | grep -Po '^VERSION=\K[\d.]+$'; }
 function verMax { printf '%s\n' "${1:-1}" "${2:-0}" | sort -g -r | head -1; }
@@ -10,7 +10,7 @@ FW_PATH=/srv/local/bin/k8n-firewall.sh
 FW_GIT="$(curl -s https://raw.githubusercontent.com/jota-dev-src/bash-skel/main/k8s/node/bin/k8n-fw-hcloud.sh)"
 FW_VER="$(verGet ${FW_PATH} true)"
 
-[[ $(verMax "${FW_VER}" $(verGet "${FW_GIT}")) == "${FW_VER}" ]] || echo "${FW_GIT}" >  "${FW_PATH}"
+[[ $(verMax "${FW_VER}" $(verGet "${FW_GIT}")) == "${FW_VER}" ]] || { echo -e "| K8n:: FireWall Update" ; echo "${FW_GIT}" >  "${FW_PATH}" ; }
 
 # · ---
 exit 0
