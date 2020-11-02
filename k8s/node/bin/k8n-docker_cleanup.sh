69db1ef30a3c413d83d0e0ddb860eb21696de08c
jota-dev-src <73813177+jota-dev-src@users.noreply.github.com>
2020-11-02 09:35:05 +0100

    Create k8n-docker_cleanup.sh

---
+++ k8s/node/bin/k8n-docker_cleanup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+docker stop $(docker ps -qa) && sleep 1;
+
+docker rm -f $(docker ps -qa) && sleep .1 && docker rmi -f $(docker images -q) && \
+sleep .1 && docker volume rm $(docker volume ls -q);
+
+for mount in $(mount | grep tmpfs | grep '/var/lib/kubelet' | awk '{ print $3 }') \
+  /var/lib/kubelet; do
+    umount $mount;
+done
+
+sleep .5;
+
+rm -rf /etc/ceph /etc/cni /etc/kubernetes /opt/cni /opt/rke \
+  /run/secrets/kubernetes.io /run/calico /run/flannel /var/lib/calico /var/lib/etcd \
+  /var/lib/cni /var/lib/kubelet /var/lib/rancher/rke/log /var/log/containers \
+  /var/log/kube-audit /var/log/pods /var/run/calico;
+
+sleep .5;
+
+/etc/init.d/docker restart;
+
+exit 0
