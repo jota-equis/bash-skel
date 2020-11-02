fb930428ffa305839c9bb9772b6fb321b87ed945
jota-dev-src <73813177+jota-dev-src@users.noreply.github.com>
2020-11-02 14:25:51 +0100

    Update k8n-fw-hcloud.sh

--- k8s/node/bin/k8n-fw-hcloud.sh
+++ k8s/node/bin/k8n-fw-hcloud.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+exec 1> >(logger -s -t $(basename $0)) 2>&1
+# · ---
 VERSION=0.9
-
+# · ---
 MASTER="${1}";
 TOKEN="${2}";
 SSH_PORT="${3:-22}";
@@ -105,4 +107,6 @@ done
 
 rm ${FRUL} && mv ${FNEW} ${FCUR}
 # · ---
+echo -e "| K8n :: Firewall rules updated"
+
 exit 0
