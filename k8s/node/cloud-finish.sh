370a965343a76a6494a424470b42b59ac68b7f02
jota-dev-src <73813177+jota-dev-src@users.noreply.github.com>
2020-11-02 22:59:14 +0100

    Update cloud-finish.sh

--- k8s/node/cloud-finish.sh
+++ k8s/node/cloud-finish.sh
@@ -62,7 +62,7 @@ systemctl enable fail2ban;
 
 ( crontab -l | grep -v -F 'k8n-firewall.sh' ; echo "*/5 * * * * /srv/local/bin/k8n-firewall.sh" ) | crontab -
 # · ---
-apt -y full-upgrade && apt -y autoclean && apt -y autoremove && sync;
+sync && apt -y full-upgrade && apt -y autoclean && apt -y autoremove && sleep 30;
 systemctl restart ssh;
 # · ---
 echo -e "| CLOUD-FINISH ... :: end :: ..."
