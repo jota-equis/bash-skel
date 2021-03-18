19e479891441401a40926f6f597aa405c250d39c
JoTa <73813177+jota-equis@users.noreply.github.com>
2021-03-18 18:34:25 +0100

    Update k8n-update.sh

--- k8s/node/bin/k8n-update.sh
+++ k8s/node/bin/k8n-update.sh
@@ -1,36 +1,47 @@
 #!/usr/bin/env bash
 exec 1> >(logger -s -t $(basename $0)) 2>&1
 # · ---
-VERSION=1.07
+VERSION=1.10
 # · ---
 function verGet { [[ $# -gt 0 ]] && ( [[ -z "${2}" ]] && echo "${1}" || echo "$(cat ${1})" ) | grep -Po '^VERSION=\K[\d.]+$'; }
 function verMax { printf '%s\n' "${1:-1}" "${2:-0}" | sort -g -r | head -1; }
+# · ---
+# UPDATER
+LOCAL_PATH=/srv/local/bin/k8n-update.sh
+LOCAL_VER="$(verGet ${LOCAL_PATH} true)"
+GIT_PATH="$(curl -s https://raw.githubusercontent.com/jota-dev-src/bash-skel/main/k8s/node/bin/k8n-update.sh)"
 
+if [[ $(verMax "${LOCAL_VER}" $(verGet "${GIT_PATH}")) != "${LOCAL_VER}" ]]; then
+    echo "${GIT_PATH}" >  "${LOCAL_PATH}";
+    echo "| K8n:: Self Update. Reloading ...";
+    sleep 1;
+    ${LOCAL_PATH};
+    exit  0;
+fi
 # · ---
 # FW
-FW_PATH=/srv/local/bin/k8n-firewall.sh
-FW_GIT="$(curl -s https://raw.githubusercontent.com/jota-dev-src/bash-skel/main/k8s/node/bin/k8n-fw-hcloud.sh)"
-FW_VER="$(verGet ${FW_PATH} true)"
+LOCAL_PATH=/srv/local/bin/k8n-firewall.sh
+LOCAL_VER="$(verGet ${LOCAL_PATH} true)"
+GIT_PATH="$(curl -s https://raw.githubusercontent.com/jota-dev-src/bash-skel/main/k8s/node/bin/k8n-fw-hcloud.sh)"
 
-if [[ $(verMax "${FW_VER}" $(verGet "${FW_GIT}")) != "${FW_VER}" ]]; then
-    echo "${FW_GIT}" >  "${FW_PATH}";
-
-    if [[ -f "/srv/local/etc/.env/TOKEN" ]]; then
-        TOKEN="$(cat /srv/local/etc/.env/TOKEN)"
-        [[ -z "${TOKEN}" ]] || sed -i "/^TOKEN=/c\TOKEN=${TOKEN}" /srv/local/bin/k8n-firewall.sh;
-    fi
-
-    if [[ -f "/srv/local/etc/.env/SSH_PORT" ]]; then
-        SSH_PORT="$(cat /srv/local/etc/.env/SSH_PORT)"
-        [[ -z "${SSH_PORT}" ]] || sed -i "/^SSH_PORT=/c\SSH_PORT=${SSH_PORT}" /srv/local/bin/k8n-firewall.sh
-    fi
-
+if [[ $(verMax "${LOCAL_VER}" $(verGet "${GIT_PATH}")) != "${LOCAL_VER}" ]]; then
+    echo "${GIT_PATH}" >  "${LOCAL_PATH}";
     echo "| K8n:: FireWall Update. Reloading ...";
     sleep 2;
     yes | ufw reset;
     sleep 1;
-    ${FW_PATH};
+    ${LOCAL_PATH};
+fi
+# · ---
+# Docker clean
+LOCAL_PATH=/srv/local/bin/k8n-docker_cleanup.sh
+GIT_PATH="$(curl -s https://raw.githubusercontent.com/jota-dev-src/bash-skel/main/k8s/node/bin/k8n-docker_cleanup.sh)"
+LOCAL_VER="$(verGet ${LOCAL_PATH} true)"
+
+if [[ $(verMax "${LOCAL_VER}" $(verGet "${GIT_PATH}")) != "${LOCAL_VER}" ]]; then
+    echo "${GIT_PATH}" >  "${LOCAL_PATH}";
+    echo "| K8n:: Docker Cleaner Updated.";
+    sleep 1;
 fi
 
-# · ---
 exit 0
